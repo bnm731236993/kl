db8bda69a31d237fac110aa146388b3d67a66fb1
bnm731236993 <bnm731236993@outlook.com>
2024-03-20 21:28:29 +0800

    测试

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='kl',
+    version='0.0.1',
+    description="My project",
+    url='https://github.com/bnm731236993/kl/tree/we731236993',
+    author_email="bnm731236993@outlook.com",
+)
