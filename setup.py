c69c1d94301b60bbf84d51e95fda30ac1950ee68
bnm731236993 <bnm731236993@outlook.com>
2024-03-21 16:05:31 +0800

    更新 setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
 from setuptools import setup
-from setuptools import find_packages
 
 setup(
     name='kl',
-    version='0.0.1',
-    description="My project",
-    url='https://github.com/bnm731236993/kl/tree/we731236993',
-    author_email="bnm731236993@outlook.com",
-    packages=find_packages(),
+    version='0.0.2',
+    description="KL project",
+    url='https://github.com/bnm731236993/kl',
+    author_email="bnm731236993@outlook.com"
 )
