d7183b14c63027eda8d75678f47bc9467ac84ed5
bnm731236993 <bnm731236993@outlook.com>
2025-04-02 21:39:19 +0800

    0.2.0-beta

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='kl',
-    version='0.1.7',
-    description="KL project",
-    url='https://github.com/bnm731236993/kl',
-    author_email="bnm731236993@outlook.com"
-)
